46ef5e958bbe5c90d4784b664545d05298b30b04
DelbertCV <dfcustodiovargas@gmail.com>
2015-07-30 08:34:07 -0600

    Modificacion al Driver

--- Driver.java
+++ Driver.java
@@ -3,7 +3,7 @@
  */
 
 /**
- * @author usuario
+ * @author Delbert Custodio -> 14246, Jonathan Aguirre -> 14349, Samuel Diaz -> 14083
  *
  */
 
@@ -13,21 +13,28 @@ import java.util.*;
 
 public class Driver {
 	
-	String DireccionArchivo;
-	BufferedReader LectorBuffer;
-	FileReader FileR;
-	Calc Calcu;
-	Stack1 Pila;
-	Scanner Scan;
-	//Hola
+	public static void main (String args[] ){
+		
+		String DireccionArchivo;
+		BufferedReader LectorBuffer;
+		FileReader FileR;
+		Calc Calcu;
+		Stack1 Pila;
+		Scanner Scan = new Scanner(System.in);
+		
+	System.out.println("Universidad del Valle de Guatemala \n Algoritmos y Estructuras de Datos\n");
+	System.out.println("Seccion 10 \n Delbert Custodio, 14246 \n Jonathan Aguirre, 14349 \n Samuel Diaz, 14083");
+	
+	System.out.println();
+	
+	System.out.println("Por favor, ingrese la direccion de su archivo de texto: \n");
+	System.out.println("Tomar en cuenta que si se encuentra en la misma carpeta que el main\n se puede escribir solo el nombre del archivo.");
+	
+	DireccionArchivo = Scan.nextLine();
+	
+	System.out.println(DireccionArchivo);
 	
 	
-	public void main (String args[] ){
-		
-		
-		
-		
-		
 		
 	}
 
